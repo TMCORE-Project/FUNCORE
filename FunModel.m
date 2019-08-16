29e5091e14edc90ed81d8a1a5fc92f12578e7b57
Zhou Lilong <dwyanechou@qq.com>
2019-08-16 18:12:04 +0800

    Update matrix generation

--- FunModel.m
+++ FunModel.m
@@ -2,7 +2,7 @@ clc
 clear
 
 mesh_file = 'x1.40962.grid.modified.nc';
-nSample   = 60; % Number of sample points
+nSample   = 100; % Number of sample points
 
 mesh = get_mesh(mesh_file,nSample);
 
@@ -12,5 +12,6 @@ for iCell = 1: mesh.nCells
     z   = mesh.zCell  (mesh.kdtree(iCell,:));
     lon = mesh.lonCell(mesh.kdtree(iCell,:));
     lat = mesh.latCell(mesh.kdtree(iCell,:));
-    w = gen_weights(x,y,z,lon,lat,mesh.a,7,7);
+    
+    w   = gen_weights(x,y,z,lon,lat,mesh.a,7,2);
 end
