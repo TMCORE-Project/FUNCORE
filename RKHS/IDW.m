df49b8b708b5402c8ebfe8c7b69a833bd71ed8e5
Zhou Lilong <dwyanechou@qq.com>
2019-08-19 18:29:02 +0800

    Construct RKHS

--- RKHS/IDW.m
+++ RKHS/IDW.m
@@ -34,6 +34,25 @@ ys=reshape(yc,[],1);
 vs=reshape(vc,[],1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% rbf
+rbf_base = @(eps_rbf,r) exp(-(eps_rbf.*r).^2);
+eps_rbf  = 0.5;
+r_1d     = pdist([xs,ys]);
+r        = squareform(r_1d);
+phi      = rbf_base(eps_rbf,r);
+c        = phi \ vs;
+
+vs_ext            = vs;
+vs_ext(end+1)     = 0;
+phi_ext           = phi;
+phi_ext(end+1,:)  = 1;
+phi_ext(:,end+1)  = 1;
+phi_ext(end,end)  = 0;
+c_ext             = phi_ext \ vs_ext;
+c                 = c_ext(1:end-1);
+
+% rbf_val = rbf(xs, ys, vs, x, y, 'gaussian',0,-4);
+
 if  strcmp(r1,'fr')
     if  (r2<=0)
         disp('Error: Radius must be positive')
@@ -44,6 +63,7 @@ if  strcmp(r1,'fr')
             D = sqrt((x(i)-xs).^2 +(y(j)-ys).^2);
             vcc = vs(D<r2);
             D   = D(D<r2);
+            cf  = c(D<r2);
             wV  = vcc.*bf(D,eps,bf_opt);
             w   = bf(D,eps,bf_opt);
             
@@ -56,7 +76,11 @@ if  strcmp(r1,'fr')
             if isempty(D)
                 wV=NaN;
             else
-                wV=sum(wV)/sum(w);
+                if bf_opt == 4
+                    wV=sum(wV.*sum(cf.*rbf_base(eps_rbf,D)))/sum(w);
+                else
+                    wV=sum(wV)/sum(w);
+                end
             end
             
             Vint(i,j)=wV;
@@ -73,6 +97,7 @@ else
             D= sqrt((x(i)-xs).^2 +(y(j)-ys).^2);
             [D,I]=sort(D);
             vcc = vs(I);
+            cf  = c(1:r2);
             wV  = vcc(1:r2).*bf(D(1:r2),eps,bf_opt);
             w   = bf(D(1:r2),eps,bf_opt);
             
@@ -82,7 +107,11 @@ else
                 w  = 1;
             end
             
-            wV  = sum(wV)/sum(w);
+            if bf_opt == 4
+                wV=sum(wV)/sum(w);
+            else
+                wV=sum(wV.*sum(cf.*rbf_base(eps_rbf,D)))/sum(w);
+            end
             
             Vint(i,j)=wV;
         end
@@ -94,19 +123,31 @@ end
 
 % base function
 function f = bf(r,eps,bf_opt)
-if bf_opt == 1
+if bf_opt == 1 || bf_opt == 4
+    if eps > 0
+        disp('eps must be negative for inverse distance scheme')
+        reture
+    end
     
     f = r.^eps;
     
 elseif bf_opt == 2
+    if eps > 0
+        disp('eps must be positive for gaussian scheme')
+        reture
+    end
     
-    f= exp(-(eps*r).^2) ./ r.^2;
+    f= exp(-(eps*r).^2);
     
 elseif bf_opt == 3
+    if eps > 0
+        disp('eps must be negative for inverse distance scheme')
+        reture
+    end
     
     dr = eps - r;
     dr(dr<0) = 0;
     
-    f = ( dr ./ (eps*r) ).^4;
+    f = ( dr ./ (eps*r) ).^2;
 end
 end
