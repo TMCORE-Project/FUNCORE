0c5e2c7b00c64d1c3ba7267389a949ab4f5b8b8b
DwyaneChou <dwyanechou@qq.com>
2019-08-22 00:36:23 +0800

    Update order check 2d

--- RKHS/gen_weights_2d.m
+++ RKHS/gen_weights_2d.m
@@ -1,65 +1,69 @@
-function w = gen_weights_2d (x,y,m,d,base_opt)
+function [w,idx] = gen_weights_2d (pts_i,pts_c,eps,poly_deg,nSamples,base_opt)
 % Input parameters
-% x,y,z Column vectors with stencil node locations; approximation to
-% be accurate at x(1),y(1),z(1)
-% m Power of r in RBF fi(r) = r^m, with m odd, >= 3.
-% d Degree of supplementary polynomials (d = -1 no polynomials)
+% pts_i Column vectors with stencil node locations; approximation to
+% be accurate at pts_c
+% eps Power of r in RBF fi(r) = r^m, with m odd, >= 3.
+% poly_deg Degree of supplementary polynomials (d = -1 no polynomials)
 % base_opt, 1 for r^m base, 2 for Gaussian base
 % Output parameter
 % w Matrix with three columns, containing weights for d/dlon, d/dlat
 
-xd = x-x(1);
-yd = y-y(1);% Shift nodes so stencil centered at origin
+x  = pts_i(:,1);
+y  = pts_i(:,2);
+xc = pts_c(:,1);
+yc = pts_c(:,2);
 
-n = length(xd);%x
+x  = x - xc;
+y  = y - yc;
+
+dist = sqrt(x.^2 + y.^2);
+
+[~,idx_tmp] = sort(dist);
+rd          = dist(idx_tmp(1:nSamples));
+idx         = idx_tmp(1:nSamples);
+
+xd = x(idx); % Chosen points x coordinate
+yd = y(idx); % Chosen points x coordinate
+
+pts_d = pts_i(idx,:);
+
+n = length(x);%x
 
 % ------ RBF part --------------------------------------------------------
-coord    = [x,y];
-r_1d     = pdist(coord);
-r        = squareform(r_1d);
+r_1d = pdist(pts_d);
+r    = squareform(r_1d);
 
 % Choose base function
 if base_opt == 1
-    phi      = r.^m; % RBF matrix
-    dphidx   = -m .* r(1,:)'.^(m-2) .* xd;
-    dphidy   = -m .* r(1,:)'.^(m-2) .* yd;
+    phi      = r.^eps;
+    dphidx   = -eps .* rd.^(eps-2) .* xd;
+    dphidy   = -eps .* rd.^(eps-2) .* yd;
 elseif base_opt == 2
-    phi      = exp(-(m.*r).^2);
-    dphidx   = -2 * m^2.*exp(-(m.* r(1,:)').^2) .* xd;
-    dphidy   = -2 * m^2.*exp(-(m.* r(1,:)').^2) .* yd;
+    phi      = exp(-(eps.*r).^2);
+    dphidx   = 2 * eps^2.*exp(-(eps.* rd).^2) .* xd;
+    dphidy   = 2 * eps^2.*exp(-(eps.* rd).^2) .* yd;
 end
 
 L0 = [dphidx,dphidy]; % RHSs
 
 % ------ Polynomial part -------------------------------------------------
-if d == -1 % Special case; no polynomial terms,
+if poly_deg == -1 % Special case; no polynomial terms,
     A = phi;
     L = L0; % i.e. pure RBF
 else % Create matrix with polynomial terms and matching constraints
-    X = xd(:,ones(1,d+1)); X(:,1) = 1; X = cumprod( X,2);
-    Y = yd(:,ones(1,d+1)); Y(:,1) = 1; Y = cumprod( Y,2);
+    X = xd(:,ones(1,poly_deg+1)); X(:,1) = 1; X = cumprod( X,2);
+    Y = yd(:,ones(1,poly_deg+1)); Y(:,1) = 1; Y = cumprod( Y,2);
     
-    np       = (d+1)*(d+2)/2; % Number of polynomial terms
-    XY       = zeros(n,np);
+    np       = (poly_deg+1)*(poly_deg+2)/2; % Number of polynomial terms
+    XY       = zeros(nSamples,np);
     col      = 1; % Assemble polynomial matrix block
-    for k = 0:d
+    for k = 0:poly_deg
         XY(:,col:col+k) = X(:,k+1:-1:1).*Y(:,1:k+1);
         col = col+k+1;
     end
     
     L1 = zeros(np,2); % Create matching RHSs
-    if d >= 1; L1(2,1) = 1; L1(3,2) = 1; end
-    
-    %     if d >= 1
-    %         L1(2,1) = 1;
-    %         L1(3,2) = 1;
-    %     end
-    %     if d >= 2
-    %         L1(4 ,1) = 2 * x(1);
-    %         L1(5 ,1) = y(1);
-    %         L1(5 ,2) = x(1);
-    %         L1(6 ,2) = 2 * y(1);
-    %     end
+    if poly_deg >= 1; L1(2,1) = 1; L1(3,2) = 1; end
     
     A = [phi,XY;XY',zeros(np)]; % Assemble linear system to be solved
     L = [L0;L1]; % Assemble RHSs
@@ -67,6 +71,6 @@ end
 
 % ------ Solve for weights -----------------------------------------------
 W = A\L;
-w = W(1:n,:); % Extract the RBF-FD weights
+w = W(1:nSamples,:); % Extract the RBF-FD weights
 
 % save mtx_my
--- RKHS/order_check_2d.m
+++ RKHS/order_check_2d.m
@@ -1,47 +1,36 @@
-clc
-clear
+% clc
+% clear
 
 eps       = 5;
-base_type = 1;
-poly_deg  = 5;
+base_type = 2;
+poly_deg  = -1;
+nSamples  = 501;
 
-res(1) = 0.25;
-res(2) = 0.5;
-res(3) = 1;
-res(4) = 2;
+minRes    = 0.05;
+nRes      = 5;
+
+res(1) = minRes;
+for ires = 2:nRes
+    res(ires) = 2*res(ires-1);
+end
 
 d2r = pi/180;
 r2d = 180/pi;
 
-% Select base function
-if base_type == 1
-    rbf_base = @(r,eps) exp(-(eps.*r).^2);
-    rbf_base_deriv = @(r,eps,xd) -2*eps^2.*exp(-(eps.*r).^2).*xd;
-elseif base_type == 2
-    rbf_base = @(r,eps) r.^eps;
-    rbf_base_deriv = @(r,eps,xd) eps * r.^(eps-2).*xd;
-elseif base_type == 3
-    rbf_base = @(r,eps) sqrt(1+(eps.*r).^2);
-    rbf_base_deriv = @(r,eps,xd) eps.^2 ./ sqrt(1+(eps*r).^2).*xd;
-end
-
-for ires = 1:size(res,2)
+for ires = 1:nRes
     x1d = -10:res(ires):10;
     y1d = -10:res(ires):10;
     
     nx = size(x1d,2);
     ny = size(y1d,2);
+    np = nx*ny;
     
     [x2d,y2d] = meshgrid(x1d,y1d);
     
     x = reshape(x2d,[],1);
     y = reshape(y2d,[],1);
     
-    x(1) = 0;
-    y(1) = 0;
-    
-    w1 = gen_weights_2d         (x,y,eps,poly_deg,base_type);
-    w2 = RBF_FD_PHS_pol_weights (x,y,eps,poly_deg);
+    pts_i = [x,y];
     
     f   = 1 + sin(4*x) + cos(3*x) + sin(2*y);
     f2d = reshape(f,nx,ny);
@@ -49,11 +38,32 @@ for ires = 1:size(res,2)
     dfdx  = 4*cos(4*x) - 3*sin(3*x);
     dfdy  = 2*cos(2*y);
     
-    dfdx1(ires) = sum(w1(:,1).*f);
-    dfdx2(ires) = sum(w2(:,1).*f);
-end
+    dfdx_2d = reshape(dfdx,nx,ny);
+    dfdy_2d = reshape(dfdy,nx,ny);
+    
+    dfdx_n = zeros(np,1);
+    dfdy_n = zeros(np,1);
+    parfor i = 1:np
+        disp(['Calculate derivative weights for point # ',num2str(i)])
+        pts_c = [x(i),y(i)];
+        xc    = x(i);
+        yc    = y(i);
+        
+        dist     = sqrt((x-xc).^2 + (y-yc).^2);
 
-L2_old = abs(dfdx1-dfdx(1))/abs(dfdx(1));
-L2_new = abs(dfdx2-dfdx(1))/abs(dfdx(1));
-order_old  = log(L2_old(2)/L2_old(1))/log(2);
-order_new  = log(L2_new(2)/L2_new(1))/log(2);
+        [w,idx]  = gen_weights_2d (pts_i,pts_c,eps,poly_deg,nSamples,base_type);
+        
+        dfdx_n(i) = sum(w(:,1).*f(idx));
+        dfdy_n(i) = sum(w(:,2).*f(idx));
+    end
+    dfdx_n_2d = reshape(dfdx_n,nx,ny);
+    dfdy_n_2d = reshape(dfdy_n,nx,ny);
+    
+    L2_x(ires) = sqrt(sum((dfdx_n - dfdx).^2) ./ sum(dfdx.^2));
+    L2_y(ires) = sqrt(sum((dfdx_n - dfdx).^2) ./ sum(dfdx.^2));
+    
+    if ires >= 2
+        order_x(ires-1) = log(L2_x(ires)/L2_x(ires-1))/log(2);
+        order_y(ires-1) = log(L2_y(ires)/L2_y(ires-1))/log(2);
+    end
+end
