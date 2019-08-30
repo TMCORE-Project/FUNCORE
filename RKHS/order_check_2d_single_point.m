991312ca7a7fd9587314a0f1190e6e15995cee13
DwyaneChou <dwyanechou@qq.com>
2019-08-30 19:46:52 +0800

    Update order check 2d

--- RKHS/order_check_2d_single_point.m
+++ RKHS/order_check_2d_single_point.m
@@ -1,13 +1,13 @@
 clc
 clear
 
-eps       = 2;
+eps       = 8;
 base_type = 2;
-poly_deg  = 6;
-nSamples  = 36;
+poly_deg  = 45;
+nSamples  = 49;
 
-minRes    = 0.1;
-nRes      = 3;
+minRes    = 1;
+nRes      = 2;
 
 res(1) = minRes;
 for ires = 2:nRes
@@ -41,26 +41,28 @@ for ires = 1:nRes
     dfdx_2d = reshape(dfdx,nx,ny);
     dfdy_2d = reshape(dfdy,nx,ny);
     
-    dfdx_n = zeros(np,1);
-    dfdy_n = zeros(np,1);
-    parfor i = 1:np
-        disp(['Calculate derivative weights for point # ',num2str(i)])
-        pts_c = [x(i),y(i)];
-        xc    = x(i);
-        yc    = y(i);
-        
-        dist     = sqrt((x-xc).^2 + (y-yc).^2);
-
-        [w,idx]  = gen_weights_2d (pts_i,pts_c,eps,poly_deg,nSamples,base_type);
-        
-        dfdx_n(i) = sum(w(:,1).*f(idx));
-        dfdy_n(i) = sum(w(:,2).*f(idx));
-    end
-    dfdx_n_2d = reshape(dfdx_n,nx,ny);
-    dfdy_n_2d = reshape(dfdy_n,nx,ny);
+%     dfdx_n = zeros(np,1);
+%     dfdy_n = zeros(np,1);
+    %     parfor i = 1:np
+    i = find(x==0&y==0);
+    disp(['Calculate derivative weights for point # ',num2str(i)])
+    pts_c = [x(i),y(i)];
+    xc    = x(i);
+    yc    = y(i);
     
-    L2_x(ires) = sqrt(sum((dfdx_n - dfdx).^2) ./ sum(dfdx.^2));
-    L2_y(ires) = sqrt(sum((dfdx_n - dfdx).^2) ./ sum(dfdx.^2));
+    dist     = sqrt((x-xc).^2 + (y-yc).^2);
+    
+    [w,idx]  = gen_weights_2d (pts_i,pts_c,eps,poly_deg,nSamples,base_type);
+    
+    dfdx_n = sum(w(:,1).*f(idx));
+    dfdy_n = sum(w(:,2).*f(idx));
+    
+    %     end
+%     dfdx_n_2d = reshape(dfdx_n,nx,ny);
+%     dfdy_n_2d = reshape(dfdy_n,nx,ny);
+    
+    L2_x(ires) = abs(dfdx_n - dfdx(i)) ./ abs(dfdx(i).^2);
+    L2_y(ires) = abs(dfdx_n - dfdx(i)) ./ abs(dfdx(i).^2);
     
     if ires >= 2
         order_x(ires-1) = log(L2_x(ires)/L2_x(ires-1))/log(2);
