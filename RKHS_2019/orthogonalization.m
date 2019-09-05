a04955c58d68f94dece7805616a7a2a3b78ce57b
DwyaneChou <dwyanechou@qq.com>
2019-09-06 02:02:13 +0800

    Upload RKHS_2019

--- RKHS_2019/orthogonalization.m
+++ RKHS_2019/orthogonalization.m
@@ -1,14 +1,44 @@
-function [e,beta] = orthogonalization(a)
-% Stabilized Gram Schmidt method for orthonormalization
-n    = size(a,2);
-b    = zeros(n,n);
-e    = zeros(n,n);
-beta = zeros(n,n);
-for i = 1:n
-   b(:,i) = a(:,i);
-   for j = 1:i-1
-       beta(i,j) = dot(b(:,j),a(:,i)) / dot(b(:,j),b(:,j));
-       b   (:,i) = b(:,i) - beta(i,j) * b(:,j);
-   end
-   e(:,i) = b(:,i) / norm(b(:,i));
+function [v,beta] = orthogonalization(v)
+%% Stabilized Gram Schmidt method for orthonormalization
+% Input is a matrix including k column vector k = 2,3,...
+% Output is the same matrix but the vectors are replaced with orthonormal
+% vectors
+%
+% Example: 
+%
+% consider two vectors v1=[3;1] , v2=[2;2]
+% we first put them in a matrix as the vectors are set column-wise
+% v = [3 2;1 2]
+%
+% A = GramSchmidt(v)
+%
+% A =
+%     0.9487   -0.3162
+%     0.3162    0.9487
+%
+% test to make sure it is correct
+%
+% dot(A(:,1),A(:,2))
+%
+% ans =
+%     0
+%
+% -------------------------------------------------
+% code by: Reza Ahmadzadeh (reza.ahmadzadeh@iit.it)
+% -------------------------------------------------
+k    = size(v,2);
+beta = zeros(k,k);
+assert(k>=2,'The input matrix must include more than one vector.');
+for ii = 1:1:k
+    v(:,ii) = v(:,ii) / norm(v(:,ii));
+    for jj = ii+1:1:k
+        [beta(ii,jj),w] = proj(v(:,ii),v(:,jj));
+        v(:,jj) = v(:,jj) - w;
+    end
+end
+    function [beta,w] = proj(u,v)
+        % This function projects vector v on vector u
+        beta = (dot(u,v) / dot(u,u));
+        w    = beta * u;
+    end
 end
